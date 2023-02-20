0fe087a1bcd9404c0450dcaacd841a4b8593ac7e
William Magnabosco <williamlodea@gmail.com>
2023-02-19 20:40:34 -0400

    a

---
+++ dom/desafio-04/app.js
@@ -0,0 +1,33 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		className: null,
+		isLoad04: false,
+		isLoad05: false,
+		colorNumber05: "red",
+		className05: null,
+		initiateLoading: false,
+	},
+	methods: {
+		iniciarEfeito() {
+		
+			const element = document.getElementById("efeito");
+			element.classList.add("destaque");
+			setInterval(() => {
+				element.classList.toggle("encolher");
+			}, 500);
+
+		},
+		iniciarProgresso() {
+			this.initiateLoading = true;
+		}
+	},
+
+	computed: {
+		styleNumber05(){
+			return {
+				backgroundColor: this.colorNumber05,
+			}
+		}
+	}
+})
