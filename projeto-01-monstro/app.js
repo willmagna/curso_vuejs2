75e6fd321662cff5119c2d04960a7d6a02c7fd23
William Magnabosco <williamlodea@gmail.com>
2023-03-19 22:49:37 -0400

    a

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -16,8 +16,23 @@ new Vue({
             this.playerLife = 100;
             this.monsterLife = 100;
         },
+        attack(special){
+            this.hurt('playerLife', 7, 12, false);
+            this.hurt('monsterLife', 5, 10, special);
+        },
+        hurt(prop, min, max, special){
+            const plus = special ? 5 : 0;
+            const hurt = this.getRandom(min + plus, max + plus);
+            this[prop] = Math.max(this[prop] - hurt, 0);
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value);
+        }
     },
     watch: {
-
+        hasResult(value){
+            if (value) this.running = false;
+        },
     }
 });
