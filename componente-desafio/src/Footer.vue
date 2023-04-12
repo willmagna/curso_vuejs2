a36ce894fc4e753fb4cc9c34335a97a56b20de02
William Magnabosco <williamlodea@gmail.com>
2023-04-11 22:23:34 -0400

    a

---
+++ componente-desafio/src/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+
+	<v-footer class="pa-3" app>
+		<v-spacer></v-spacer>
+		<div>Curso Vue Desafio &copy; {{ new Date().getFullYear() }}</div>
+	</v-footer>
+
+</template>
+
+<script>
+
+export default {
+
+	data() {
+		return {
+		}
+	}
+}
+</script>
